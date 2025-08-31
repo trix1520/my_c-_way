2da37daaacc5cec3e660d8dbcc754cfc2585d2df
Trix <isidjdj15@gmail.com>
2025-08-31 14:09:02 +0300

    user-defining typing

---
+++ class/class.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    class Person {
+        public:
+            std::string name;
+            int age;
+            
+            void printInfo() {
+                std::cout << "Name: " << name << ", Age: " << age << '\n';
+            };
+    };
+    
+    Person person {"Panteleimon", 16};
+    person.printInfo();
+    
+    return 0;
+}
