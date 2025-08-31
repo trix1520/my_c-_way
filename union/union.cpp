5d6f36ae78f04f1713b9f8077c7a96d2e8fb5af4
Trix <isidjdj15@gmail.com>
2025-08-31 16:33:30 +0300

    User-Defined Data Types

---
+++ union/union.cpp
@@ -0,0 +1,26 @@
+#include <iostream> 
+
+int main() {
+    union Data {
+        int num;
+        char letter;
+        float decimal;
+    };
+    
+    Data data;
+    data.num = 42;
+    std::cout << "Data as integer: " << data.num << '\n';
+    data.letter = 'A';
+    std::cout << "Data as character: " << data.letter << '\n';
+    data.decimal = 3.14f;
+    std::cout << "Data as float: " << data.decimal << '\n';
+    void* pNum; 
+    pNum = static_cast<int*>(&data.num);
+    std::cout << "Data interpreted as integer: " << &pNum << '\n';
+    pNum = static_cast<float*>(&data.decimal);
+    std::cout << "Data interpreted as float: " << &pNum << '\n';
+    pNum = static_cast<char*>(&data.letter);
+    std::cout << "Data interpreted as character: " << &pNum << '\n';
+
+    return 0;
+}
